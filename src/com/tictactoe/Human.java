103a9503c5e3993bd5d551cf512803da71c57cae
Merce Bauza <merce.bauza@gmail.com>
2018-05-12 03:26:09 +0100

    Created the exceptions and other changes

--- src/com/tictactoe/Human.java
+++ src/com/tictactoe/Human.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,21 +14,40 @@ public class Human extends Player {
   public static Scanner input = new Scanner(System.in); // the input Scanner
 
   public int getSpot(List<String> board, Player player1, Player player2, Player currentPlayer) {
-    System.out.print(currentPlayer.getName()+ ", please enter a number between 0 and 8:\n");
-    int spot = input.nextInt();
-    return spot;
+    System.out.print(currentPlayer.getName() + ", please enter a number between 1 and 9:\n");
+    int spot = 0;
+    boolean validInput = false;
+    do {
+      try {
+        if (input.hasNextInt()) {
+          spot = input.nextInt();
+        } else {
+          throw new InputMismatchException("You can only input integers");
+        }
+
+        if (spot > 0 && spot < 10) {
+           validInput = true;
+           spot = spot - 1;
+        } else {
+           throw new IllegalArgumentException("You can only input an integer between 1 and 9");
+        }
+
+      }
+      catch (InputMismatchException ex) {
+        System.out.println(ex.getMessage());
+        input.next();
+        validInput = false;
+      }
+
+      catch (IllegalArgumentException ex) {
+        System.out.println(ex.getMessage());
+        validInput = false;
+      }
+
+  } while(!validInput);
+
+  return spot;
+
   }
-
-  public void chooseName() {
-    System.out.print("Enter the player name:\n");
-    super.setName(input.next());
-  }
-
-  public void chooseSymbol() {
-    System.out.print("Enter the player symbol:\n");
-    super.setSymbol(input.next());
-  }
-
-
 
 }
--- src/com/tictactoe/Main.java
+++ src/com/tictactoe/Main.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -26,79 +27,146 @@ public class Main {
     int choiceEntry = 0;
     int type = 0;
 
-    while(choiceEntry > -1 || choiceEntry < 5) {
+    while(choiceEntry > -1 && choiceEntry < 5) {
 
       System.out.println("Enter \"1\" to Play, \"2\" to Choose opponent, \"3\" to Change names or \"4\" to Change symbols");
-      if (input.hasNextInt())
-        choiceEntry = input.nextInt();
 
-      switch (choiceEntry) {
+      try {
+        if (input.hasNextInt()) {
+          choiceEntry = input.nextInt();
+        } else {
+          throw new InputMismatchException("You can only input integers");
+        }
 
-        case 1:
-          startNewGame();
-          do {
-            if (!threeInLine() && !tie()) {
-              evalBoard();
-            }
-          } while (!threeInLine() && !tie()); // repeat if not game-over
-          System.out.print("Game over\n");
-          break;
-
-
-        case 2:
-          System.out.println("Enter \"1\" to play against another Player or \"2\" to play against the Computer");
-          if (input.hasNextInt()) {
-            type = input.nextInt();
-
-            if (type == 1) {
-              player2 = new Human("Player 2", "O");
-            } else {
-              player2 = new Computer("Computer", "O");
-            }
-
-            System.out.println("You are playing against" + player2.getName());
-          }
-          break;
-
-        case 3:
-          if (player2 instanceof Computer) {
-            changeName(player1, player2);
-          } else {
-            System.out.println("Enter \"1\" to change " + player1.getName() + "'s name or \"2\" to change " + player2.getName() + "'s name");
-            if (input.hasNextInt()) {
-              int num = input.nextInt();
-
-              if (num == 1) {
-                changeName(player1, player2);
-              } else {
-                changeName(player2, player1);
-              }
-            }
-          }
-        break;
-
-        case 4:
-          if (player2 instanceof Computer) {
-            changeSymbol(player1, player2);
-          } else {
-            System.out.println("Enter \"1\" to change " + player1.getName() + "'s symbol or \"2\" to change " + player2.getName() + "'s symbol");
-            if (input.hasNextInt()) {
-              int num = input.nextInt();
-
-              if (num == 1) {
-                changeSymbol(player1, player2);
-              } else {
-                changeSymbol(player2, player1);
-              }
-            }
-          }
-        break;
+        if (choiceEntry < 1 || choiceEntry > 4) {
+          throw new IllegalArgumentException("You can only input an integer between 1 and 4");
+        }
+      } catch (IllegalArgumentException ex) {
+        System.out.println(ex.getMessage());
+        choiceEntry = 0;
+      } catch (InputMismatchException ex) {
+        System.out.println(ex.getMessage());
+        input.next();
       }
-     }
+
+      boolean validInput = false;
+      do {
+        switch (choiceEntry) {
+
+          case 1:
+            startNewGame();
+            do {
+              if (!threeInLine() && !tie()) {
+                evalBoard();
+              }
+            } while (!threeInLine() && !tie()); // repeat if not game-over
+            System.out.print("Game over\n");
+            break;
+
+
+          case 2:
+            System.out.println("Enter \"1\" to play against another Player or \"2\" to play against the Computer");
+            try {
+              if (input.hasNextInt()) {
+                type = input.nextInt();
+              } else {
+                throw new InputMismatchException("You can only input integers");
+              }
+
+              if (type == 1) {
+                player2 = new Human("Player 2", "O");
+                validInput = true;
+              } else if (type == 2) {
+                player2 = new Computer("Computer", "O");
+                validInput = true;
+              } else {
+                throw new IllegalArgumentException("You can only input an integer between 1 and 2");
+              }
+              System.out.println("You are playing against " + player2.getName());
+            } catch (IllegalArgumentException ex) {
+              System.out.println(ex.getMessage());
+              validInput = false;
+            } catch (InputMismatchException ex) {
+              System.out.println(ex.getMessage());
+              validInput = false;
+              input.next();
+            }
+
+            break;
+
+          case 3:
+            if (player2 instanceof Computer) {
+              changeName(player1, player2);
+              validInput = true;
+            } else {
+              System.out.println("Enter \"1\" to change " + player1.getName() + "'s name or \"2\" to change " + player2.getName() + "'s name");
+              int num;
+              try {
+                if (input.hasNextInt()) {
+                  num = input.nextInt();
+                } else {
+                  throw new InputMismatchException("You can only input integers");
+                }
+
+                if (num == 1) {
+                  validInput = changeName(player1, player2);
+                } else if (num == 2) {
+                  validInput = changeName(player2, player1);
+                } else {
+                  throw new IllegalArgumentException("You can only input an integer between 1 and 2");
+                }
+              } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+                validInput = false;
+
+              } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+                validInput = false;
+                input.next();
+              }
+
+            }
+            break;
+
+          case 4:
+            if (player2 instanceof Computer) {
+              changeSymbol(player1, player2);
+              validInput = true;
+            } else {
+              System.out.println("Enter \"1\" to change " + player1.getName() + "'s symbol or \"2\" to change " + player2.getName() + "'s symbol");
+              int num;
+              try {
+                if (input.hasNextInt()) {
+                  num = input.nextInt();
+                } else {
+                  throw new InputMismatchException("You can only input integers");
+                }
+
+                if (num == 1) {
+                  validInput = changeSymbol(player1, player2);
+                } else if (num == 2) {
+                  validInput = changeSymbol(player2, player1);
+                } else {
+                  throw new IllegalArgumentException("You can only input an integer between 1 and 2");
+                }
+              } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+                validInput = false;
+              } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+                validInput = false;
+                input.next();
+              }
+            }
+            break;
+        }
+
+      } while (!validInput);
+    }
   }
 
 
-  private static void changeName(Player player1, Player player2) {
+  private static boolean changeName(Player player1, Player player2) {
     System.out.println("Enter new name for " + player1.getName());
     if (input.hasNext()) {
       String name = input.next();
@@ -106,11 +174,16 @@ public class Main {
       if (!(player2.getName().equals(name))) {
         player1.setName(name);
         System.out.println("Name changed to " + player1.getName());
+        return true;
+      }else{
+        System.out.println("The name you are trying to change is the same one as your opponent's name: " + player2.getName());
+        return false;
       }
     }
+    return false;
   }
 
-  private static void changeSymbol(Player player1, Player player2) {
+  private static boolean changeSymbol(Player player1, Player player2) {
     System.out.println("Enter new symbol for " + player1.getName());
     if (input.hasNext()) {
       String symbol = input.next();
@@ -118,8 +191,13 @@ public class Main {
       if (!(player2.getSymbol().equals(symbol))) {
         player1.setSymbol(symbol);
         System.out.println("Symbol changed to " + player1.getSymbol());
+        return true;
+      }else{
+        System.out.println("The symbol you are trying to change is the same one as your opponent's symbol: " + player2.getName());
+        return false;
       }
     }
+    return false;
   }
 
   /** Initializes the game */
@@ -166,6 +244,8 @@ public class Main {
       board.set(spot,currentPlayer.getSymbol());
       currentPlayer = nextPlayer();
       printBoard();
+    } else {
+      System.out.println("Enter a number that is not already used");
     }
   }
 
