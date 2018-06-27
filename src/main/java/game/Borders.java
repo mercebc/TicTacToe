c3e7170acc21ed793096ba9339505a776dc6e122
Merce Bauza <merce.bauza@gmail.com>
2018-06-27 23:15:12 +0100

    correct interface borders

--- src/main/java/game/Borders.java
+++ src/main/java/game/Borders.java
@@ -1,13 +1,8 @@
 package game;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public interface Borders {
-  HashMap<Integer, Integer> position = null;
-  List<Cell> cells = null;
-
 
   public List<Cell> getCells();
 
